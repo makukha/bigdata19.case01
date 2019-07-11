d9ace7ffeb9a9a2600252da5348aa9ee21379a1c
Michael Makukha <m.makukha@gmail.com>
2019-07-11 20:44:05 +0300

    Updated requirements

--- yahoo.py
+++ yahoo.py
@@ -1,6 +1,8 @@
 import csv
 from pathlib import Path
 
+import config as cfg
+
 
 DATA = Path('data')
 
@@ -23,6 +25,31 @@ def read_symbols():
                 symbols.add(row['Symbol'].upper().strip())
 
     return list(sorted(symbols))
+
+def scrape_descriptions(bq, table_html):
+    """"""
+
+    symbols = read_symbols()
+    progress = tqdm(total=len(symbols))
+    cfg.YAHOO_HTMLS.mkdir(parents=True, exist_ok=True)
+
+
+    async def fetch(symbol, session):
+        async with session.get(f'https://finance.yahoo.com/quote/{symbol}/profile?p={symbol}') as response:
+            text = await response.read()
+            async with aiofiles.open(cfg.YAHOO_HTMLS / f'{symbol}.html', 'wb') as f:
+                f.write(text)
+            progress.update(1)
+
+    async def run(symbols):
+        async with ClientSession() as session:
+            tasks = (asyncio.ensure_future(fetch(symbol, session)) for symbol in symbols)
+            responses = await asyncio.gather(*tasks)
+
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.ensure_future(run(symbols)))
+    progress.close()
 
 
 def main():
