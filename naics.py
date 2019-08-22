da0134f0d30ea3c2ce6f809a1c7954a1542f86aa
Mykhailo Makukha <m.makukha@gmail.com>
2019-08-22 19:57:42 +0300

    Updated main model

--- naics.py
+++ naics.py
@@ -74,10 +74,13 @@ def main():
         .addGrid(logistic_wordcount.elasticNetParam, [0.0, 0.1, 0.2, 0.3])
         .build()
         )
+    evaluator = MulticlassClassificationEvaluator(predictionCol='prediction', metricName='accuracy')
     cv = CrossValidator(
         estimator=logistic_wordcount,
         estimatorParamMaps=grid,
         numFolds=5,
+        evaluator=evaluator,
+        seed=100500,
         )
     model_cv = cv.fit(prepared)
     breakpoint()
